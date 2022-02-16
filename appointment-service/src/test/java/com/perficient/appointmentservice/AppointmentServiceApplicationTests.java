39a94002013f5c0dbf8f15872798d7d65eaa1ef3
David Athreton <ather041@umn.edu>
2022-02-16 10:29:12 -0600

    Innitial Commit

---
+++ appointment-service/src/test/java/com/perficient/appointmentservice/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perficient.appointmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
