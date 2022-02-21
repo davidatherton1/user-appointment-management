f0594de32c588535971b0dd0aea1eaf7e7f31743
David Athreton <ather041@umn.edu>
2022-02-21 13:11:51 -0600

    Added Management Service

--- management-service/src/main/java/com/perficient/managementservice/ManagementServiceApplication.java
+++ management-service/src/main/java/com/perficient/managementservice/ManagementServiceApplication.java
@@ -2,8 +2,9 @@ package com.perficient.managementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ManagementServiceApplication {
 
 	public static void main(String[] args) {
