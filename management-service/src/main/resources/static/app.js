dd2b2954607ea790bc8da586cf2fbe3d6f3bd1b3
Luis Matute <luis.matute@perficient.com>
2022-02-28 11:18:48 -0600

    Basic table population
    Populates the user's table, or should. I currently can't test

--- management-service/src/main/resources/static/app.js
+++ management-service/src/main/resources/static/app.js
@@ -34,6 +34,44 @@ function loadUserTable(){
     })
 }
 
+function loadUsersTable() {
+    $.ajax({
+        url: "http://localhost:8081/management/users",
+        jsonp: "callback",
+        dataType: "jsonp",
+        headers: {'Access-Control-Allow-Origin': "*"},
+
+        success: function(response) {
+            var tableData = $(`<tr>
+                        <th>First Name</th>
+                        <th>Last name</th>
+                        <th>Gender</th>
+                        <th>Age</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                    </tr>`);
+
+
+            response.forEach(user => {
+                tableData.append(`<tr data-id='${user["id"]}'>
+                    <td>${user["firstName"]}</td>
+                    <td>${user["lastName"]}</td>
+                    <td>${user["gender"]}</td>
+                    <td>${user["age"]}</td>
+                    <td>${user["emailAddress"]}</td>
+                    <td>${user["phoneNumber"]}</td>
+                </tr>`);
+            });
+
+            $("#user-table").html(tableData);
+        },
+
+        error: function(response) {
+            console.log(response);
+        }
+
+    });
+}
 
 function onLoadUserInfo(){
 
