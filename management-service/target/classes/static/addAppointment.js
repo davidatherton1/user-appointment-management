4587d579c14b24b4cdd4e1bbb4ca9c30bf5504de
Luis Matute <luis.matute@perficient.com>
2022-03-02 14:28:45 -0600

    Auto stash before rebase of "origin/Feature/UserService"

---
+++ management-service/target/classes/static/addAppointment.js
@@ -0,0 +1,27 @@
+$.get("http://localhost:8082/management/users", function (data){
+    let selectElement = document.getElementById("user")
+    data.forEach(element => {
+        let name = element.firstName + " " + element.lastName
+        selectElement.add(new Option(name, element.id))
+    })
+})
+
+function addAppt(){
+    let formData = {
+        apptName: $("#apptName").val(),
+        apptType: $("#apptType").val(),
+        description: $("#description").val(),
+        user: $("#user").val()
+    }
+    console.log(formData)
+
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        type: "POST",
+        url: "http://localhost:8082/management/appointment/",
+        data: JSON.stringify(formData),
+        dataType: "json"
+    })}
