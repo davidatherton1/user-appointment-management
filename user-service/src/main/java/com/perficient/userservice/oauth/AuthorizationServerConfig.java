40d17a626caa36bba3f1cf24090f5d48628861b9
Luis Matute <luis.matute@perficient.com>
2022-03-03 10:33:03 -0600

    Added dependencies for OAuth

---
+++ user-service/src/main/java/com/perficient/userservice/oauth/AuthorizationServerConfig.java
@@ -0,0 +1,15 @@
+package com.perficient.userservice.oauth;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+//@Import(OAuth2AuthorizationServer)
+public class AuthorizationServerConfig {
+
+//    @Bean
+//    public RegisteredCl
+
+}
